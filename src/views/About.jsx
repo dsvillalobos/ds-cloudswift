63c45980eeb03513b8f985fe3b54ab6242d6460c
Sebastian Villalobos <dsvillacorta@gmail.com>
2024-09-11 22:16:45 -0600

    DS CloudSwift | Version 2.0.4
    Release Notes:
    
    - New "CloudSwift AI".
    - New PWA Implementation.
    - Bug Fixes.

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -32,7 +32,7 @@ function About() {
               <span className="fw-bold">Created by</span> @dsvillalobos
             </li>
             <li className="list-group-item">
-              <span className="fw-bold">Version</span> 2.0.3
+              <span className="fw-bold">Version</span> 2.0.4
             </li>
             <li className="list-group-item">
               <span className="fw-bold">Help & Privacy</span>
